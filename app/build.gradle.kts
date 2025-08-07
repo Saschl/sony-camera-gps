ee20b45ecfdae550848090a25a43e3cd71e3688c
Saschl <sascharudolf46@gmail.com>
2025-08-07 12:29:18 +0200

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.saschl.sonygps"
         minSdk = 36
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.2.0"
+        versionCode = 11
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
